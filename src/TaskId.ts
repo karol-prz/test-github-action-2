c1572f8e266faeffe58f1f34766644a47a98bf9e
Karol <karol.przestrzelski@teamwork.com>
2023-01-09 18:36:47 +0000

    test14

--- src/TaskId.ts
+++ src/TaskId.ts
@@ -1,27 +1,9 @@
-import * as github from "@actions/github"
 import * as core from '@actions/core'
 
-export type ClientType = ReturnType<typeof github.getOctokit>;
-
 export async function getTaskId(): Promise<string | undefined> {
-    // const prNumber = getPrNumber()
-    // if (!prNumber) {
-    //     core.debug(`Unable to find current PR number`)
-    //     return undefined
-    // }
-
-    // const pullRequest = await client.rest.pulls.get({
-    //     owner: github.context.repo.owner,
-    //     repo: github.context.repo.repo,
-    //     pull_number: prNumber,
-    // })
-
-    // let branchName = pullRequest.data.head.ref
-
     const branchName = process.env.GITHUB_HEAD_REF?.replace('refs/heads/', '');
-    core.debug(`Branch name is ${branchName}`)
     if (!branchName) {
-        throw Error("Branch name is undefined")
+        throw Error("Branch name is not found, is this being run in a pull request?")
     }
 
     let taskIdRegex = new RegExp("\/([0-9]+)\/")
@@ -34,13 +16,4 @@ export async function getTaskId(): Promise<string | undefined> {
         core.debug(`Found task id ${taskId}`)
         return taskId
     }
-}
-
-function getPrNumber(): number | undefined {
-    const pullRequest = github.context.payload.pull_request;
-    if (!pullRequest) {
-        return undefined;
-    }
-    
-    return pullRequest.number;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,13 @@
 import * as core from '@actions/core'
 import * as github from "@actions/github"
 import { getBuildVersionNumber } from './VersionNumber'
-import { ClientType, getTaskId } from './TaskId'
+import { getTaskId } from './TaskId'
 import { sendWebHook } from './SendWebHook'
 
 async function run(): Promise<void> {
   try {
     const buildVersionFile: string = core.getInput('buildVersionFile')
     const buildVersionRegex: string = core.getInput('buildVersionRegex')
-    const token = core.getInput("repo-token", { required: true })
-
-    const client: ClientType = github.getOctokit(token)
 
     const buildVersionNumber: string = await getBuildVersionNumber(buildVersionFile, buildVersionRegex)
     const taskId = await getTaskId()
