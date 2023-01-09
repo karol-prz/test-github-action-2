db6c8ff2a03ba93cc2858bfa74b6c50db3c1d98d
Karol <karol.przestrzelski@teamwork.com>
2023-01-09 10:18:48 +0000

    test2

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,9 @@ import * as core from '@actions/core'
 async function run(): Promise<void> {
   try {
     const buildVersionFile: string = core.getInput('buildVersionFile')
+    core.debug('Hello World')
     core.debug(`Waiting getting file ${buildVersionFile}`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-    
+
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
