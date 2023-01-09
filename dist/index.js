c8acd7866751034c7196018b51f7ad7c56790a7a
Karol <karol.przestrzelski@teamwork.com>
2023-01-09 13:50:26 +0000

    test14

--- dist/index.js
+++ dist/index.js
@@ -66,7 +66,7 @@ function run() {
                 pull_number: prNumber,
             });
             const branchName = pullRequest.data.head.ref;
-            const taskIdRegex = new RegExp("([0-9]+)");
+            const taskIdRegex = new RegExp("\/([0-9]+)\/");
             const taskIdMatch = taskIdRegex.exec(branchName);
             if (taskIdMatch == null) {
                 core.debug(`Unable to find task id in branch name ${branchName}`);
