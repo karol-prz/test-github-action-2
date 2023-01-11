faf95cfd8cb1e0ed2cfefa586e60464ec856d76e
Karol <karol.przestrzelski@teamwork.com>
2023-01-11 15:16:10 +0000

    Changing to url query params

--- src/SendWebHook.ts
+++ src/SendWebHook.ts
@@ -1,16 +1,12 @@
 import fetch from 'node-fetch';
 
-const WEBHOOK_URL = "https://hooks.zapier.com/hooks/catch/11378468/bjrvyc0/"
+const WEBHOOK_URL = "https://hooks.zapier.com/hooks/catch/11378468/bjrvyc0?"
 
 export async function sendWebHook(buildVersionNumber: string, taskId: string): Promise<void> {
-    let params = {
+    let params = new URLSearchParams({
         buildVersionNumber: buildVersionNumber,
-        taskId: taskId
-    };
-    
-    fetch(WEBHOOK_URL, {
-        method: 'POST',
-        body: JSON.stringify(params),
-        headers: { 'Content-Type': 'application/json' }
+        taskId: taskId,
     })
+    
+    fetch(WEBHOOK_URL + params)
 }
