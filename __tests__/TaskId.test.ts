d8e038cdbcbd2d49d7b4a507a1f2bb15cde2ee99
Karol <karol.przestrzelski@teamwork.com>
2023-01-11 16:37:43 +0000

    Reenabling workflows

---
+++ __tests__/TaskId.test.ts
@@ -0,0 +1,38 @@
+import * as core from '@actions/core';
+import { getTaskId } from '../src/TaskId';
+import { expect, test, jest, describe, beforeEach } from '@jest/globals';
+
+describe('getTaskId', () => {
+  beforeEach(() => {
+    // Reset the process.env object before each test
+    process.env = {};
+  });
+
+  test('should throw an error if the branch name is not found', async () => {
+    await expect(getTaskId()).rejects.toThrow('Branch name is not found, is this being run in a pull request?');
+  });
+
+  test('should return the task ID if it is found in the branch name', async () => {
+    process.env.GITHUB_HEAD_REF = 'refs/heads/test/12345/bug_fix';
+    await expect(getTaskId()).resolves.toBe('12345');
+  });
+
+  test('should return undefined if the task ID is not found in the branch name', async () => {
+    process.env.GITHUB_HEAD_REF = 'refs/heads/test/bug_fix';
+    await expect(getTaskId()).resolves.toBe(undefined);
+  });
+
+  test('should debug the task ID if it is found in the branch name', async () => {
+    process.env.GITHUB_HEAD_REF = 'refs/heads/test/12345/bug_fix';
+    jest.spyOn(core, 'debug').mockImplementation(() => {});
+    await getTaskId();
+    expect(core.debug).toHaveBeenCalledWith('Found task id 12345 w');
+  });
+
+  test('should debug a message if the task ID is not found in the branch name', async () => {
+    process.env.GITHUB_HEAD_REF = 'refs/heads/test/bug_fix';
+    jest.spyOn(core, 'debug').mockImplementation(() => {});
+    await getTaskId();
+    expect(core.debug).toHaveBeenCalledWith('Unable to find taskId in branch name test/bug_fix');
+  });
+});
---
+++ __tests__/VersionNumber.test.ts
@@ -0,0 +1,20 @@
+import { expect, test, jest } from '@jest/globals';
+import { readFileSync } from 'fs'
+import { getBuildVersionNumber } from '../src/VersionNumber';
+
+test('getBuildVersionNumber throws error when regex does not match', async () => {
+  const mockReadFileSync = jest.fn().mockReturnValue('file contents');
+  (readFileSync as any) = mockReadFileSync;
+
+  await expect(getBuildVersionNumber('file.txt', 'invalid-regex')).rejects.toThrow(
+    'Unable to find version number in "file.txt" with regex: invalid-regex'
+  );
+});
+
+test('getBuildVersionNumber returns build version number', async () => {
+  const mockReadFileSync = jest.fn().mockReturnValue('version: 1.0.0');
+  (readFileSync as any) = mockReadFileSync;
+
+  const buildVersionNumber = await getBuildVersionNumber('file.txt', 'version: (\\d+\\.\\d+\\.\\d+)');
+  expect(buildVersionNumber).toEqual('1.0.0');
+});
